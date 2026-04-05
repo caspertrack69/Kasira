dd8f0f2615ab9a7c73efa8c602e0ba21d7762716
Casper <ar886069@gmail.com>
2026-04-05 12:10:53 +0700

    Add base controller helpers for authorization

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * Common base controller wiring Laravel helpers for all application controllers.
+ */
+abstract class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests;
+    use DispatchesJobs;
+    use ValidatesRequests;
 }
